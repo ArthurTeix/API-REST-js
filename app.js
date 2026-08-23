f13f7c265c6883d6d6ec3d69e7fc52be7099e54c
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-22 22:48:29 -0300

    refactor: refact code

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// O Eslint e o Editorconfig servem para padronizar o código e evitar erros de sintaxe.
-// Eles padronizam identação, aspas, ponto e vírgula, entre outros aspectos do código.
 
-const oi = 3;
-console.log(oi);
-if (oi === 3) {
-  console.log("digitou 3");
-}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// O Eslint e o Editorconfig servem para padronizar o código e evitar erros de sintaxe.
+// Eles padronizam identação, aspas, ponto e vírgula, entre outros aspectos do código.
+
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
