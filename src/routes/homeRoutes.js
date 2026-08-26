98cab82e1b62cbea27e4d93eea8395263f85cd0a
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-25 22:29:57 -0300

    feat: import home controller

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import homeController from "../controllers/HomeController"; // import com letra minúscula para importar a classe e não o objeto que vem no default
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-  res.send("Olá muundo");
-});
+router.get("/", homeController.index);
 
 export default router;
