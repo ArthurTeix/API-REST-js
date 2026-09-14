fa5d9164b84001142a129d2afec18ee286e30cb0
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-14 19:26:07 -0300

    feat: create connection database

---
+++ src/database/index.js
@@ -0,0 +1,10 @@
+// a função desse arquivo vai ser enviar o dados ao banco de dados
+import Sequelize from "sequelize";
+import databaseConfig from "../config/database"
+import Aluno from "../models/Aluno";
+
+const models = [Aluno]
+
+const connection = new Sequelize(databaseConfig)
+
+models.forEach(model => model.init(connection))
