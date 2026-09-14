013873aec6f9f45d1423dbf8d1c74482dd6c37cc
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-14 19:27:04 -0300

    feat: execute database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+import "./src/database"; // executando arquivo automaticamente para garantir os dados
+
 import express from "express";
 import homeRoutes from "./src/routes/homeRoutes";
 
